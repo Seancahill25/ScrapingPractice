81a04082773a9e8335368faf5dba057aec85c2c1
Sean Cahill <seancahill25@gmail.com>
2019-01-31 23:50:58 -0500

    some more practice

---
+++ API_Parsing/API_Parsing/Program.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using RestSharp.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace API_Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/posts");
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            var posts = JArray.FromObject(JsonConvert.DeserializeObject(content));
+
+
+            Console.WriteLine(posts[0]["title"]);
+        }       
+    }
+}
